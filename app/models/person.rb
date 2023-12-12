5bf6b1981f19d0e24e5a35195551487845e802a3
sohosoki <so.hosoki@enechange.co.jp>
2023-12-12 18:46:21 +0900

    array

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,21 +11,29 @@ class Person
   sig { returns(Address) }
   attr_accessor :address
 
+  sig { returns(T::Array[Address]) }
+  attr_reader :address_history
+
   sig { params(name: String, age: Integer, email: String, address: Address).void }
   def initialize(name, age, email, address)
     @name = name
     @age = age
     @email = email
     @address = address
+    @address_history = [address]
   end
 
   sig { params(new_address: Address).void }
   def move_to(new_address)
     @address = new_address
+    @address_history << new_address
   end
 
   sig { returns(String) }
   def to_s
-    "名前：#{@name}, 年齢：#{@age}, メール：#{@email}, 住所：#{@address}"
+    "名前：#{@name}, 年齢：#{@age}, メール：#{@email}
+住所：
+  #{@address_history.join("\n  ")}
+"
   end
 end

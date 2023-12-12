887ee75cbdee7eab461e4652541109ae2b40ccda
sohosoki <so.hosoki@enechange.co.jp>
2023-12-12 18:42:51 +0900

    typed

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,20 @@
+# typed: true
+require 'sorbet-runtime'
+
 class Address
+  extend T::Sig
+
+  sig { returns(String) }
   attr_accessor :street, :city, :postal_code
 
+  sig { params(street: String, city: String, postal_code: String).void }
   def initialize(street, city, postal_code)
     @street = street
     @city = city
     @postal_code = postal_code
   end
 
+  sig { returns(String) }
   def to_s
     "住所：#{@street}, 市町村：#{@city}, 郵便番号：#{@postal_code}"
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,17 @@
+# typed: true
 class Person
-  attr_accessor :name, :age, :email, :address
+  extend T::Sig
 
+  sig { returns(String) }
+  attr_accessor :name, :email
+  
+  sig { returns(Integer) }
+  attr_accessor :age
+  
+  sig { returns(Address) }
+  attr_accessor :address
+
+  sig { params(name: String, age: Integer, email: String, address: Address).void }
   def initialize(name, age, email, address)
     @name = name
     @age = age
@@ -8,10 +19,12 @@ class Person
     @address = address
   end
 
+  sig { params(new_address: Address).void }
   def move_to(new_address)
     @address = new_address
   end
 
+  sig { returns(String) }
   def to_s
     "名前：#{@name}, 年齢：#{@age}, メール：#{@email}, 住所：#{@address}"
   end
